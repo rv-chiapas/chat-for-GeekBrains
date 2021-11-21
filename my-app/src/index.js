8f8427f928042f8ec3b89ed31eb1a02ac459271d
[chiapas] <[r.v.chiapas@gmail.com]>
2021-11-21 16:39:58 +0200

    tesat commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <React.StrictMode>
+    <h1>Hello, world!</h1>
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
