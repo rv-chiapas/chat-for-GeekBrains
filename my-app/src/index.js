d9d5626c8aaac2e82a68d7b12d7401a476f8c426
[chiapas] <[r.v.chiapas@gmail.com]>
2021-11-21 20:41:59 +0200

    commit 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+const Message = (props) => {
+  return (
+    <div>
+      <h1 class="heading">{props.text}</h1>
+      <p class="paragraph">{props.paragr}</p>
+    </div>
+  );
+};
+
+const App = () => {
+ return  (
+      <Message />
+  )};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello, world!</h1>
+  <React.StrictMode> 
+    <Message text="Здесь мы будем создавать чат!" paragr='Чат создаём в рамках изучения библиотеки React' />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
-
